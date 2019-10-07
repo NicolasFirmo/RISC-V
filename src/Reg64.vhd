0f6a9c5b16577014758b46cadd5bede1bff258d7
Cefas Rodrigues Freire <cefas.rodrigues@lais.huol.ufrn.br>
2019-10-07 10:23:45 -0300

    Refatoração de registrador

--- src/Reg64.vhd
+++ src/Reg64.vhd
@@ -1,19 +1,25 @@
-library ieee ;
-use ieee . std_logic_1164 .all;
-entity Reg64 is
-port (clk,c,ld: in std_logic ;
-		d: in std_logic_vector(63 downto 0);
-		q : out std_logic_vector(63 downto 0));
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Reg64 is port (
+	clk, ld : in std_logic;
+	d		: in std_logic_vector(63 downto 0);
+	q		: out std_logic_vector(63 downto 0)
+);
 end Reg64;
+
 architecture ckt of Reg64 is
-signal qs: std_logic_vector(63 downto 0);
+
+	signal qs: std_logic_vector(63 downto 0) := (others => '0');
+
 begin
-process (clk,c)
-begin
-if c = '0' then qs <= "0000000000000000000000000000000000000000000000000000000000000000";
-elsif clk ='1' and clk ' event and ld = '1' then
-qs <= d;
-end if;
-end process ;
-q <= qs;
+	
+	process (clk, ld)
+	begin
+		if clk ='1' and clk'event and ld = '1' then
+			qs <= d;
+		end if;
+	end process ;
+	
+	q <= qs;
 end ckt;
